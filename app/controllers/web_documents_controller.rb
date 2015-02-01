d1c201fc50841e743e26ed5817ad568cfb6d0bdb
Dmitry Novikov <glowinglens@gmail.com>
2015-02-01 23:23:40 +0300

    Delete old files

--- app/controllers/web_documents_controller.rb
+++
@@ -1,9 +0,0 @@
-class WebDocumentsController < ApplicationController
-  rescue_from ActionView::MissingTemplate, with: :not_found
-
-  def show
-    @document = GetWebDocument.(params[:path])
-    @document.match RawDocument, &method(:render)
-  end
-end
-
--- app/services/get_web_document.rb
+++
@@ -1,9 +0,0 @@
-module GetWebDocument module_function
-  def call(request_path)
-    [WebDocument, RawDocument].reduce(nil) do |res, model|
-      res || model.parse(request_path)
-    end
-    # Value[request_path] | WebDocument | RawDocument
-  end
-end
-
