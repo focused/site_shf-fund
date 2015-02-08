4936e416bf8bcdc06c8b4d551c71dd9783837110
Dmitry Novikov <glowinglens@gmail.com>
2015-02-08 22:58:20 +0300

    Fix again

--- app/controllers/upmin/active_record/query.rb
+++ app/controllers/upmin/active_record/query.rb
@@ -6,13 +6,14 @@ module Upmin::ActiveRecord
 
       res = res.where(real: true) if klass.model_class.attribute_names.include?("real")
 
-      if klass.model_class.attribute_names.include?("position")
-        res.order(:parent_id, :position)
-      else
-        res.order(:parent_id)
-      end
 
+      if klass.model_class.attribute_names.include?("position")
+        res = res.order(:parent_id, :position)
+      else
+        res = res.order(:parent_id)
+      end
       res = res.order(created_at: :desc) if klass.model_class.attribute_names.include?("created_at")
+
       res
     end
 
