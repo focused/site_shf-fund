0ed9e427b551d82c043464df6330204ab404a25d
Dmitry Novikov <glowinglens@gmail.com>
2015-02-08 22:24:19 +0300

    Fixes

--- app/controllers/catalog/products_controller.rb
+++ app/controllers/catalog/products_controller.rb
@@ -1,5 +1,6 @@
 class Catalog::ProductsController < ApplicationController
   def show
     @document = Catalog::ProductDocument.(params[:path], params[:path_id])
+    session[:last_catalog_path] = request.path
   end
 end
