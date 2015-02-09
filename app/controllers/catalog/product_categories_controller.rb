5deedd9fc90581998afe85afe9b0b3a898204cc5
Dmitry Novikov <glowinglens@gmail.com>
2015-02-09 09:08:34 +0300

    Fix session url

--- app/controllers/catalog/product_categories_controller.rb
+++ app/controllers/catalog/product_categories_controller.rb
@@ -1,5 +1,6 @@
 class Catalog::ProductCategoriesController < ApplicationController
   def show
     @document = Catalog::ProductCategoryDocument.(params[:path])
+    session[:last_catalog_path] = request.path
   end
 end
--- app/controllers/catalog/products_controller.rb
+++ app/controllers/catalog/products_controller.rb
@@ -1,6 +1,5 @@
 class Catalog::ProductsController < ApplicationController
   def show
     @document = Catalog::ProductDocument.(params[:path], params[:path_id])
-    session[:last_catalog_path] = request.path
   end
 end
