a95abbd78052099fbb40e2db68af086da6f146c0
Dmitry Novikov <glowinglens@gmail.com>
2015-02-13 19:04:02 +0300

    move pdf styles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
-  after_all.js upmin/overrides.js upmin/overrides.css ckeditor/*
+  after_all.js upmin/overrides.js upmin/overrides.css ckeditor/* pdf_application.css
 )
